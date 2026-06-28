3ad97c52eef80264051bb30d2a457e0bebf8f4b7
Sathya0110 <seelansathya2195@gmail.com>
2026-06-28 21:07:42 +0530

    Initial commit

---
+++ src/SLL/Main.java
@@ -0,0 +1,14 @@
+package SLL;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Singly list=new Singly();
+        list.insertEnd(10);
+        list.insertEnd(11);
+        list.insertEnd(12);
+        list.insertEnd(13);
+        list.display();
+    }
+}
---
+++ src/Singly.java
@@ -0,0 +1,81 @@
+public class Singly {
+    private Node head;
+    private Node tail;
+    private int size=0;
+
+    public class Node
+    {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(Node next, int data) {
+            this.next = next;
+            this.data = data;
+        }
+    }
+    public void insertFirst(int data)
+    {
+        Node newNode=new Node(data);
+        if(head==null)
+        {
+            newNode.next=head;
+            head=newNode;
+            tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+        size++;
+
+    }
+    public void insertEnd(int data)
+    {
+        Node newNode=new Node(data);
+        if(head==null)
+        {
+            newNode.next=head;
+            head=newNode;
+            tail=newNode;
+            return;
+        }
+        tail.next=newNode;
+        tail=newNode;
+        size++;
+    }
+    public void deleteFirst()
+    {
+        if(head==null)
+        {
+            System.out.println("There is no element to delete");
+            return;
+        }
+        head=head.next;
+    }
+    public void deleteMiddle()
+    {
+
+    }
+    public void deleteLast()
+    {
+
+    }
+
+    public void display()
+    {
+        Node node=head;
+        while(node!=null)
+        {
+            System.out.print(node.data+"->");
+            node=node.next;
+        }
+        System.out.print("End");
+    }
+
+
+
+
+}
